39b704dfe101c4ec8454a6bd6fdc863b65ead722
hugomfcruz <mkrimson@gmail.com>
2014-06-06 17:54:21 +0100

    Update cachematrix.R
    Adding first version of implemented cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## This function creates an object that contain the matrix and,
+## if calculated, the inverse of that matrix
+## It is implemented as a list with 4 methods:
+## get() - returns the original matrix
+## set(y) - sets the original matrix
+## getinverse() - returns the inverted matrix
+## setinverse(y) - sets the inverted matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+	inverse <- NULL
+        set <- function(y) {
+                x <<- y
+                ##if matrix is changing, invalidate the cache
+                inverse <<- NULL
+        }
+        get <- function() { x }
+        setinverse <- function(inverted){ inverse <<- inverted }
+        getinverse <- function(){ inverse }
+        
+        
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## This function returns the inverted matrix
+## If the inverted matrix was calculated before, a cached copy is returned
+## Otherwise, the inverse in calculated and cached
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inverse <- x$getinverse()
+        if(!is.null(inverse)) {
+                message("getting cached data")
+                return(inverse)
+        }
+        
+        ##Calculate the inverse of the matrix
+        data <- x$get()
+        inverse <- solve(data)
+        #Add the calculated matrix to the cache
+        x$setinverse(inverse)
+        inverse
 }
+
+## testing
+#originalmatrix <- matrix(c(4, 2, 7, 6), nrow=2, ncol=2)
+#mymatrix <- makeCacheMatrix(originalmatrix)
+#cacheSolve(mymatrix)
+#cacheSolve(mymatrix)
+##change mymatrix
+#mymatrix$set(matrix(c(2, 2, 3, 2), nrow=2, ncol=2))
+#mymatrix <- cacheSolve(mymatrix)
+#mymatrix <- cacheSolve(mymatrix)
